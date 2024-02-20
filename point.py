06d40129c21c3baa96f2fdefaeb64c173cac0e45
Awwab <mawwabkhank2006@gmail.com>
2024-02-20 17:10:06 +0500

    string representation and round to decimal places

--- point.py
+++ point.py
@@ -6,4 +6,7 @@ class Point:
         self.y = y
 
     def calculate_distance(self, other: "Point") -> float:
-        return math.hypot(self.x - other.x, self.y - other.y)
+        return round(math.hypot(self.x - other.x, self.y - other.y), 2)
+
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
