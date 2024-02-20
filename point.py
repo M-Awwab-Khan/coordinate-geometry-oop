bb18c1a47ee2730a008a7e32733ea091a059f397
Awwab <mawwabkhank2006@gmail.com>
2024-02-20 17:27:15 +0500

    distance from origin and quadrant checker

--- point.py
+++ point.py
@@ -8,6 +8,19 @@ class Point:
     def calculate_distance(self, other: "Point") -> float:
         return round(math.hypot(self.x - other.x, self.y - other.y), 2)
 
+    def distance_from_origin(self) -> float:
+        return round(math.hypot(self.x, self.y), 2)
+
+    def which_quadrant(self) -> int:
+        if self.x > 0 and self.y > 0:
+            return 1
+        elif self.x < 0 and self.y > 0:
+            return 2
+        elif self.x < 0 and self.y < 0:
+            return 3
+        elif self.x > 0 and self.y < 0:
+            return 4
+
     def __add__(self, other: "Point") -> "Point":
         return Point(self.x + other.x, self.y + other.y)
     
