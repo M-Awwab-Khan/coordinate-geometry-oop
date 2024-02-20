72660eefdf220067c7e0f5ecbe42c64b66bb19a4
Awwab <mawwabkhank2006@gmail.com>
2024-02-20 17:07:59 +0500

    calculate distance func and type hints

--- point.py
+++ point.py
@@ -1,6 +1,9 @@
 import math
 
 class Point:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x: int = 0, y: int = 0) -> None:
         self.x = x
         self.y = y
+
+    def calculate_distance(self, other: "Point") -> float:
+        return math.hypot(self.x - other.x, self.y - other.y)
