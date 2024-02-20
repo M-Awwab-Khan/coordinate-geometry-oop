4f0f61172a63ca8fc2d935d5ff1b8e68c51d3afe
Awwab <mawwabkhank2006@gmail.com>
2024-02-20 17:18:21 +0500

    add, sub, and multiply by constant methods override

--- point.py
+++ point.py
@@ -8,5 +8,14 @@ class Point:
     def calculate_distance(self, other: "Point") -> float:
         return round(math.hypot(self.x - other.x, self.y - other.y), 2)
 
-    def __str__(self):
+    def __add__(self, other: "Point") -> "Point":
+        return Point(self.x + other.x, self.y + other.y)
+    
+    def __sub__(self, other: "Point") -> "Point":
+        return Point(self.x - other.x, self.y - other.y)
+    
+    def __mul__(self, k: int) -> "Point":
+        return Point(self.x * k, self.y * k)
+    
+    def __str__(self) -> str:
         return f"Point({self.x}, {self.y})"
