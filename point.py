563d8f67de5fb6e8a9167f202ea6f2faa795ea33
Awwab <mawwabkhank2006@gmail.com>
2024-02-20 18:55:38 +0500

    polar implementation and representation

--- point.py
+++ point.py
@@ -1,9 +1,12 @@
 import math
 
 class Point:
-    def __init__(self, x: int = 0, y: int = 0) -> None:
+    def __init__(self, x: int = 0, y: int = 0, mode='cartesian') -> None:
         self.x = x
         self.y = y
+        self.mode = mode
+        self.r = round(math.hypot(self.x, self.y), 2)
+        self.θ = round(math.atan(self.y / self.x), 2)
 
     def calculate_distance(self, other: "Point") -> float:
         return round(math.hypot(self.x - other.x, self.y - other.y), 2)
@@ -22,13 +25,13 @@ class Point:
             return 4
 
     def __add__(self, other: "Point") -> "Point":
-        return Point(self.x + other.x, self.y + other.y)
+        return Point(self.x + other.x, self.y + other.y, mode=self.mode)
     
     def __sub__(self, other: "Point") -> "Point":
-        return Point(self.x - other.x, self.y - other.y)
+        return Point(self.x - other.x, self.y - other.y, mode=self.mode)
     
     def __mul__(self, k: int) -> "Point":
-        return Point(self.x * k, self.y * k)
+        return Point(self.x * k, self.y * k, mode=self.mode)
     
     def __eq__(self, other: "Point") -> bool:
         return (self.x == other.x) and (self.y == other.y)
@@ -36,8 +39,10 @@ class Point:
     
 
     @classmethod
-    def from_polar_coords(cls, r, theta):
-        return cls(r * math.cos(theta), r * math.sin(theta))
+    def from_polar_coords(cls, r, θ):
+        return cls(r * math.cos(θ), r * math.sin(θ), mode='polar')
     
     def __str__(self) -> str:
-        return f"Point({self.x}, {self.y})"
+        if self.mode == 'cartesian':
+            return f"Point(x = {self.x}, y = {self.y})"
+        return f"Point(r = {self.r}, θ = {self.θ})"
